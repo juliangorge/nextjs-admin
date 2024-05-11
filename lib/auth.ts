336ff395997eb9a6ad0ee14e6f5acd27e65cd213
juliangorge-lrn <juliangorge@hotmail.com>
2024-05-11 14:44:24 -0300

    first commit

---
+++ lib/auth.ts
@@ -0,0 +1,32 @@
+import NextAuth from 'next-auth';
+import Google from 'next-auth/providers/google';
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import { PrismaClient } from "@prisma/client"
+
+const prisma = new PrismaClient();
+
+export const {
+    handlers: { GET, POST },
+    auth,
+    signIn,
+    signOut
+} = NextAuth({
+    adapter: PrismaAdapter(prisma),
+    session: { strategy: 'jwt' },
+    providers: [
+        Google({
+            profile(profile) : any {
+                return { role: profile.role ?? "user"}
+            },
+        })
+    ],    
+    callbacks: {
+        async signIn({ profile }) {
+            const user = await prisma.user.findUnique({
+                where: { email: profile?.email }
+            });
+
+            return user != null;
+        }
+    }
+});
